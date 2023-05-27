c380c9992cfb475b0521c6cc59ddbb74c9865a8b
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2023-05-27 02:21:03 +0200

    Add files via upload

--- ls.c
+++ ls.c
@@ -1,3 +1,6 @@
+// AUTHOR MikyTuberr / Piotr
+// 27.05.2023
+
 #include <dirent.h>
 #include <errno.h>
 #include <grp.h>
@@ -12,22 +15,41 @@
 #define PATH_SIZE 512
 #define TIME_SIZE 80
 
-// Function to display detailed information about a file/directory
-void l_option(const char *path) {
+// Check if the path is valid
+int isPathValid(const char* path) {
   DIR *pDIR;
-  struct dirent *pDirEnt;
-
   pDIR = opendir(path);
   if (pDIR == NULL) {
-    fprintf(stderr, "opendir() failed for directory '%s' (%s)\n", path,
-            strerror(errno));
-    exit(EXIT_FAILURE);
+    return 1;
   }
+  return 0;
+}
 
-  int total = 0; // Variable which contains files size
-  char full_path[PATH_SIZE];
+// Function to display file size
+void printFileSize(struct stat *file_info, const char* extraOptions) {
+  // File size
+  if (strchr(extraOptions, 'h') != NULL) {
+    if (file_info->st_size < 1024) {
+      printf(" %ldB", file_info->st_size);
+    } 
+    else if (file_info->st_size < 1024 * 1024) {
+      printf(" %.1fK", file_info->st_size / 1024.0);
+    } 
+    else if (file_info->st_size < 1024 * 1024 * 1024) {
+      printf(" %.1fM", file_info->st_size / (1024.0 * 1024));
+    } 
+    else {
+      printf(" %.1fG", file_info->st_size / (1024.0 * 1024 * 1024));
+    }
+  }
+  else {
+    printf(" %ld", file_info->st_size);
+  }
+}
 
-  // Get the size of files
+// Function to get files size
+int getSizeOfFiles(DIR* pDIR, struct dirent* pDirEnt, const char* path) {
+  int total = 0;
   while ((pDirEnt = readdir(pDIR)) != NULL) {
     if (strcmp(pDirEnt->d_name, ".") != 0 && strcmp(pDirEnt->d_name, "..") != 0 
     && pDirEnt->d_name[0] != '.') {
@@ -43,12 +65,29 @@ void l_option(const char *path) {
       total += file_info.st_blocks;
     }
   }
+  return total;
+}
 
-  printf("Total %d\n", (total/2));
+// Function to display detailed information about a file/directory
+void l_option(const char *path, const char* extraOptions) {
+  DIR *pDIR;
+  struct dirent *pDirEnt;
+  pDIR = opendir(path);
 
-   // Go back to beggining of catalog
-  rewinddir(pDIR);
+  if (pDIR == NULL) {
+    fprintf(stderr, "opendir() failed for directory '%s' (%s)\n", path,
+            strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+ 
+  // Check if option 's' was given
+  if (strchr(extraOptions, 's') != NULL) {
+    int total = getSizeOfFiles(pDIR, pDirEnt, path); // Variable which contains files size
+    printf("Total %d\n", (total/2));
+    rewinddir(pDIR); // Go back to beggining of catalog
+  }
 
+  char full_path[PATH_SIZE];
   while ((pDirEnt = readdir(pDIR)) != NULL) {
     if(pDirEnt->d_name[0] != '.') {
       //(char* str, size_t size, const char* format, first %s,  second %s)
@@ -59,6 +98,11 @@ void l_option(const char *path) {
         fprintf(stderr, "Error while reading information about file");
         exit(EXIT_FAILURE);
       }
+
+      // Check if option 's' was given
+      if (strchr(extraOptions, 's') != NULL) {
+        printf("%ld ", file_info.st_blocks);
+      } 
 
       // Type of file
       printf((S_ISDIR(file_info.st_mode)) ? "d" : "-");
@@ -83,8 +127,7 @@ void l_option(const char *path) {
       struct group *group_info = getgrgid(file_info.st_gid);
       printf(" %s %s", owner_info->pw_name, group_info->gr_name);
 
-      // File size
-      printf(" %ld", file_info.st_size);
+      printFileSize(&file_info, extraOptions);
 
       // Last modified time
       char time_buffer[TIME_SIZE];
@@ -105,7 +148,7 @@ void l_option(const char *path) {
 }
 
 // Function to recursively print the contents of a directory
-void R_option(const char *path) {
+void R_option(const char *path, const char* extraOptions) {
   DIR *pDIR;
   struct dirent *pDirEnt;
   char recursion_path[PATH_SIZE];
@@ -119,6 +162,13 @@ void R_option(const char *path) {
 
   memset(recursion_path, 0, sizeof(recursion_path));
   strcat(recursion_path, path);
+
+  // Check if option 's' was given
+  if (strchr(extraOptions, 's') != NULL) {
+    int total = getSizeOfFiles(pDIR, pDirEnt, recursion_path); // Variable which contains files size
+    printf("Total %d\n", (total/2));
+    rewinddir(pDIR); // Go back to beggining of catalog
+  }
 
   printf("%s:\n", recursion_path);
 
@@ -134,6 +184,11 @@ void R_option(const char *path) {
         exit(EXIT_FAILURE);
       }
       
+      // Check if option 's' was given
+      if (strchr(extraOptions, 's') != NULL) {
+        printf("%ld ", file_info.st_blocks);
+      }
+
       // Check if it is catalog or regular file
       if(S_ISDIR(file_info.st_mode)) {
         printf("\033[42m%s\033[0m  ", pDirEnt->d_name);
@@ -149,16 +204,16 @@ void R_option(const char *path) {
       // Check if directory is catalog
       if (pDirEnt->d_type == DT_DIR) {
         // If it's a directory, recursively call print_directory_contents
-        R_option(full_path);
+        R_option(full_path, extraOptions);
       }
     }
   }
   closedir(pDIR);
 }
 
-// Function to print contents of directory
-void default_option(const char *path) {
-  DIR *pDIR;
+// Function to print contents of directory, including hidden files (files that begin with ".")
+void a_option(const char *path, const char* extraOptions) {
+    DIR *pDIR;
     struct dirent *pDirEnt;
     pDIR = opendir(path);
     if (pDIR == NULL) {
@@ -166,6 +221,14 @@ void default_option(const char *path) {
               strerror(errno));
       exit(EXIT_FAILURE);
     }
+
+    // Check if option 's' was given
+    if (strchr(extraOptions, 's') != NULL) {
+      int total = getSizeOfFiles(pDIR, pDirEnt, path); // Variable which contains files size
+      printf("Total %d\n", (total/2));
+      rewinddir(pDIR); // Go back to beggining of catalog
+    }
+
     pDirEnt = readdir(pDIR);
     while (pDirEnt != NULL) {
       char full_path[PATH_SIZE];
@@ -175,6 +238,58 @@ void default_option(const char *path) {
       if (stat(full_path, &file_info) == -1) {
         fprintf(stderr, "Error while reading information about file");
         exit(EXIT_FAILURE);
+      }
+
+      // Check if option 's' was given
+      if (strchr(extraOptions, 's') != NULL) {
+        printf("%ld ", file_info.st_blocks);
+      }
+
+      // Check if it is catalog or regular file
+      if(S_ISDIR(file_info.st_mode)) {
+        printf("\033[42m%s\033[0m  ", pDirEnt->d_name);
+      }
+      else {
+        printf("\033[1;32m%s  \033[0m", pDirEnt->d_name);
+      }
+      
+      pDirEnt = readdir(pDIR);
+    }
+    printf("\n");
+    closedir(pDIR);
+}
+
+// Function to print contents of directory
+void default_option(const char *path, const char* extraOptions) {
+  DIR *pDIR;
+    struct dirent *pDirEnt;
+    pDIR = opendir(path);
+    if (pDIR == NULL) {
+      fprintf(stderr, "opendir() failed for directory '%s' (%s)\n", path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    pDirEnt = readdir(pDIR);
+
+    if (strchr(extraOptions, 's') != NULL) {
+      int total = getSizeOfFiles(pDIR, pDirEnt, path); // Variable which contains files size
+      printf("Total %d\n", (total/2));
+      rewinddir(pDIR); // Go back to beggining of catalog
+    }
+
+    while (pDirEnt != NULL) {
+      char full_path[PATH_SIZE];
+      snprintf(full_path, sizeof(full_path), "%s/%s", path, pDirEnt->d_name);
+
+      struct stat file_info;
+      if (stat(full_path, &file_info) == -1) {
+        fprintf(stderr, "Error while reading information about file");
+        exit(EXIT_FAILURE);
+      }
+
+      // Check if option 's' was given
+      if (strchr(extraOptions, 's') != NULL) {
+        printf("%ld ", file_info.st_blocks);
       }
 
       if(pDirEnt->d_name[0] != '.') {
@@ -194,35 +309,57 @@ void default_option(const char *path) {
 
 int main(int argc, char *argv[]) {
   const char *path;
-  
-  if (argc > 1 && strcmp(argv[1], "-l") == 0) {
+
+  if (argc > 1 && strchr(argv[1], '-') != NULL && strchr(argv[1], 'l') != NULL) {
     if (argc == 3) {
       path = argv[2];
     } 
     else {
       path = ".";
     }
-    l_option(path);
+    l_option(path, argv[1]);
   } 
-  else if (argc > 1 && strcmp(argv[1], "-R") == 0) {
+  else if (argc > 1 && strchr(argv[1], '-') != NULL && strchr(argv[1], 'R') != NULL) {
     if (argc == 3) {
       path = argv[2];
     } 
     else {
       path = ".";
     }
-    R_option(path);
+    R_option(path, argv[1]);
     printf("\n");
   } 
-  else if (argc > 1) {
-    fprintf(stderr, "Invalid command line options\n");
-    fprintf(stderr, "Usage: %s [-l|-R] [directory]\n", argv[0]);
-    exit(EXIT_FAILURE);
+  else if (argc > 1 && strchr(argv[1], '-') != NULL && strchr(argv[1], 'a') != NULL) {
+    if (argc == 3) {
+      path = argv[2];
+    } 
+    else {
+      path = ".";
+    }
+    a_option(path, argv[1]);
+  }
+  else if (argc > 1 && strchr(argv[1], '-') != NULL && strchr(argv[1], 's') != NULL) {
+    if (argc == 3) {
+      path = argv[2];
+    } 
+    else {
+      path = ".";
+    }
+    default_option(path, argv[1]);
+  }
+  else if (argc > 1 && strcmp(argv[0], "./ls") == 0 && isPathValid(argv[1]) == 0) {
+    path = ".";
+    default_option(path, "\0");
+  }
+  else if (argc == 1 && strcmp(argv[0], "./ls") == 0) {
+    path = ".";
+    default_option(path, "\0");
   }
   else {
-     path = ".";
-     default_option(path);
+    fprintf(stderr, "Invalid command line options\n");
+    fprintf(stderr, "Usage: %s [-l|-R|-a|-s] [directory]\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
-
+  
   return 0;
 }
